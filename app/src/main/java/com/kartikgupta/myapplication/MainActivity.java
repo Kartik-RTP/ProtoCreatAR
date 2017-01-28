a4e980bb6b5a867599f8c1b67d9dcc0b9e1920f8
praina <prateekraina2012@gmail.com>
2017-01-28 23:05:00 +0530

    Preference Activity Added
    Preference Added and web socket connection tested

--- app/src/main/java/com/kartikgupta/myapplication/MainActivity.java
+++ app/src/main/java/com/kartikgupta/myapplication/MainActivity.java
@@ -43,6 +43,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -69,6 +71,30 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
             }
         });
 
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void startCameraActivity() {
